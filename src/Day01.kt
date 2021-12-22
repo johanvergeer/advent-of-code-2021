251b3a0ebe985eac128b8d0aa6a5b975f98bb4a8
Johan Vergeer <johanvergeer@gmail.com>
2021-12-22 13:52:34 +0100

    Finish exercise day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,17 @@
+fun getIncreasesCount(input: List<Int>, windowSize: Int = 2): Int {
+    return input
+        .windowed(windowSize, 1)
+        .filter { p -> p.first() < p.last() }
+        .size
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>) = getIncreasesCount(input.map { it.toInt() })
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part2(input: List<String>) = getIncreasesCount(input.map { it.toInt() }, 4)
 
     val input = readInput("Day01")
+
     println(part1(input))
     println(part2(input))
 }

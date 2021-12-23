d221947830bda83745020dc5f88e2c310da03d6f
Johan Vergeer <johanvergeer@gmail.com>
2021-12-23 15:39:08 +0100

    Refactor exercise day 03

--- src/Day03.kt
+++ src/Day03.kt
@@ -17,20 +17,25 @@ private fun gammaRateBits(input: List<List<Int>>) = input
     .map { row -> row.groupingBy { it }.eachCount().maxByOrNull { it.value }?.key }
     .joinToString(separator = "")
 
-fun gammaRate(input: List<List<Int>>) = gammaRateBits(input).toInt(2)
+private fun gammaRate(input: List<List<Int>>) = gammaRateBits(input).toInt(2)
 
-fun epsilonRate(input: List<List<Int>>) = gammaRateBits(input)
+private fun epsilonRate(input: List<List<Int>>) = gammaRateBits(input)
     .invertBits()
     .toInt(2)
 
-private fun Map<Int, Int>.areValuesEqual() = values.all { it == values.first() }
+private data class BitCounts(val zeroes: Int, val ones: Int) {
+    val valueWithHighestCount = if (ones >= zeroes) 1 else 0
+    val valueWithLowestCount = if (ones > zeroes) 0 else 1
+}
 
-private fun airRating(input: List<List<Int>>, selector: (Map<Int, Int>) -> Int): Int {
+private fun Map<Int, Int>.toBitCounts() = BitCounts(this[0] ?: 0 , this[1] ?: 0)
+
+private fun airRating(input: List<List<Int>>, selector: (BitCounts) -> Int): Int {
     val rowSize = input[0].size
     var rows = input.map { row -> row.map { it } }
 
     for (i in 0 until rowSize) {
-        val itemsCountsInColumn = rows.map { it[i] }.groupingBy { it }.eachCount()
+        val itemsCountsInColumn = rows.map { it[i] }.groupingBy { it }.eachCount().toBitCounts()
 
         rows = rows.filter { it[i] == selector(itemsCountsInColumn) }
 
@@ -41,24 +46,19 @@ private fun airRating(input: List<List<Int>>, selector: (Map<Int, Int>) -> Int):
     throw IllegalStateException("Only one row should remain")
 }
 
-private fun valueWithHighestCount(itemsCountsInColumn: Map<Int, Int>) =
-    if (itemsCountsInColumn.areValuesEqual()) 1
-    else itemsCountsInColumn.maxByOrNull { it.value }?.key!!
+private fun valueWithHighestCount(itemsCountsInColumn: BitCounts) = itemsCountsInColumn.valueWithHighestCount
 
-private fun valueWithLowestCount(itemsCountsInColumn: Map<Int, Int>) =
-    if (itemsCountsInColumn.areValuesEqual()) 0
-    else itemsCountsInColumn.minByOrNull { it.value }?.key!!
+private fun valueWithLowestCount(itemsCountsInColumn: BitCounts) = itemsCountsInColumn.valueWithLowestCount
 
-fun oxygenGeneratorRating(input: List<List<Int>>) = airRating(input, ::valueWithHighestCount)
+private fun oxygenGeneratorRating(input: List<List<Int>>) = airRating(input, ::valueWithHighestCount)
 
-fun co2ScrubberRating(input: List<List<Int>>) = airRating(input, ::valueWithLowestCount)
+private fun co2ScrubberRating(input: List<List<Int>>) = airRating(input, ::valueWithLowestCount)
 
 fun part1(input: List<List<Int>>) = gammaRate(input) * epsilonRate(input)
 
 fun part2(input: List<List<Int>>) = oxygenGeneratorRating(input) * co2ScrubberRating(input)
 
 fun main() {
-
     val input = readInput("Day03")
         .map { it.toList().map { digit -> digit.digitToInt() } }
     val transposedInput = input.transpose()
